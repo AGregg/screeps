4fb30dc413c653570ef71321ccf4115e75519ed4
Andrew Gregg <AndrewGGregg@gmail.com>
2016-12-25 12:23:29 -0500

    cleanup

--- screeps/main.js
+++ screeps/main.js
@@ -26,38 +26,6 @@ module.exports.loop = function () {
     }
 }
 
-function SpawnWorker(roomName, role){
-		//work 100 energy, ability to harvest, build, repair, dismantle, upgrade
-		//move 50 energy, reduces fatigue by 2 points per tick
-		//carry 50 energy, can carry 50 resources
-		var energy = CountEnergy(roomName);
-		var body = [WORK,CARRY,MOVE];
-		energy = energy - 200;
-		while (energy >= 150){
-			body.push(WORK, MOVE);
-			energy = energy - 150;
-		}
-
-    var newName = Game.rooms[roomName].find(FIND_MY_SPAWNS)[0].createCreep(body, undefined, {role: role, caste: 'worker' } );
-    console.log('Spawning new ' + role + ': ' + newName);
-}
-
-function CountCreepsWithRole(role){
-    return _.filter(Game.creeps, (creep) => creep.memory.role == role).length;
-}
-
-function CountCreepsWithCaste(caste){
-	return _.filter(Game.creeps, (creep) => creep.memory.caste == caste).length;
-}
-
-function ActivateSafeMode(roomName){
-	if (Game.rooms[roomName] == undefined) return;
-	var hostiles = Game.rooms[roomName].find(FIND_HOSTILE_CREEPS);
-	if (hostiles.length > 0 && Game.rooms[roomName].controller.safeMode == undefined){
-		Game.rooms[roomName].controller.activateSafeMode();
-	}
-}
-
 function ClearCreepMemory(){
 	for(var name in Memory.creeps) {
         if(!Game.creeps[name]) {
