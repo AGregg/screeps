35bf78caf238f8bb48a2647f51e8313609f70768
Andrew Gregg <AndrewGGregg@gmail.com>
2016-12-27 01:17:32 -0500

    build on closest spot

--- screeps/creepFunctions.js
+++ screeps/creepFunctions.js
@@ -48,10 +48,10 @@ exports.stock = function(creep){
 }
 
 exports.build = function(creep){
-  var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-    if(targets.length) {
-        if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
-            creep.moveTo(targets[0]);
+  var target = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES);
+    if(target != null) {
+        if(creep.build(target) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(target);
         }
     } else {
       creep.memory.job = 'harvest';
